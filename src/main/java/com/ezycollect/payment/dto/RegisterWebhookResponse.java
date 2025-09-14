0dce462b1cf1c7d9b6f441bba837e6d8c38b275d
Elton Gonsalves <eltonx17@gmail.com>
2025-09-14 20:34:38 +1000

    [1.0.0] - Fixed for error handling

--- src/main/java/com/ezycollect/payment/dto/RegisterWebhookResponse.java
+++ src/main/java/com/ezycollect/payment/dto/RegisterWebhookResponse.java
@@ -7,18 +7,13 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RegisterWebhookResponse {
     private String status;
-    private String message;
+    private String error;
 
     public RegisterWebhookResponse(String status) {
         this.status = status;
     }
-
-    public RegisterWebhookResponse(String status, String message) {
-        this.status = status;
-        this.message = message;
-    }
 }
-
